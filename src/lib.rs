c482b3d9f4c1e2fd339cc4a70e8f991b4bf442d5
Alex Knauth <alexander@knauth.org>
2023-07-18 12:49:52 -0400

    Basic start and end
    TODO: make it use next scene like the C# autosplitter does, not just current scene

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ use asr::future::{next_tick, retry};
 use asr::Process;
 use asr::game_engine::unity::{SceneManager, get_scene_name};
 use asr::string::ArrayCString;
+use asr::timer::TimerState;
+// use asr::time::Duration;
 
 asr::async_main!(stable);
 // asr::panic_handler!();
@@ -27,13 +29,13 @@ async fn main() {
                 // TODO: Load some initial information from the process.
                 let scene_manager = SceneManager::wait_attach(&process).await;
                 let mut scene_name = get_scene_name_string(wait_get_current_scene_path::<SCENE_PATH_SIZE>(&process, &scene_manager).await);
-                asr::print_message(&scene_name);
+                on_new_scene(&scene_name);
                 loop {
                     // TODO: Do something on every tick.
                     if let Ok(next_scene_name) = scene_manager.get_current_scene_path::<SCENE_PATH_SIZE>(&process).map(get_scene_name_string) {
                         if next_scene_name != scene_name {
                             scene_name = next_scene_name;
-                            asr::print_message(&scene_name);
+                            on_new_scene(&scene_name);
                         }
                     }
                     next_tick().await;
@@ -49,4 +51,21 @@ async fn wait_get_current_scene_path<const N: usize>(process: &Process, scene_ma
 
 fn get_scene_name_string<const N: usize>(scene_path: ArrayCString<N>) -> String {
     String::from_utf8(get_scene_name(&scene_path).to_vec()).unwrap()
+}
+
+fn on_new_scene(scene_name: &str) {
+    asr::print_message(scene_name);
+    match asr::timer::state() {
+        TimerState::NotRunning => {
+            if scene_name == "Tutorial_01" {
+                asr::timer::start();
+            }
+        }
+        TimerState::Running => {
+            if scene_name.starts_with("Cinematic_Ending") {
+                asr::timer::split();
+            }
+        }
+        _ => ()
+    }
 }
