13039c615e9d54fa110b1b6ba1e0a7c263b5ab0c
AlexKnauth <alexander@knauth.org>
2023-11-15 00:26:29 -0500

    use parse_macro_input!

--- crates/ugly_widget_derive/src/lib.rs
+++ crates/ugly_widget_derive/src/lib.rs
@@ -1,18 +1,17 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(SetHeadingLevel)]
 pub fn set_heading_level_derive(input: TokenStream) -> TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
-    let ast = syn::parse(input).unwrap();
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
 
     // Build the trait implementation
-    impl_set_heading_level(&ast)
+    impl_set_heading_level(&input)
 }
 
-fn impl_set_heading_level(ast: &syn::DeriveInput) -> TokenStream {
+fn impl_set_heading_level(ast: &DeriveInput) -> TokenStream {
     let name = &ast.ident;
     let gen = quote! {
         impl SetHeadingLevel for #name {
