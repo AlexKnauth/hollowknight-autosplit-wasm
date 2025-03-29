ee4c8924477cf4bba3f6cc3d7a7afd5109d77d59
AlexKnauth <alexander@knauth.org>
2025-03-29 11:58:34 -0400

    Start on all_scene_names

--- src/splits.rs
+++ src/splits.rs
@@ -3877,6 +3877,7 @@ pub fn continuous_splits(
     s: &Split,
     p: &Process,
     g: &GameManagerFinder,
+    ss: &SceneStore,
     pds: &mut PlayerDataStore,
     sds: &mut SceneDataStore,
 ) -> SplitterAction {
@@ -4968,8 +4969,10 @@ pub fn continuous_splits(
         Split::OnDefeatWhiteDefender => should_split(pds.incremented_white_defender_defeats(p, g)),
         Split::WhiteDefenderStatueUnlocked => should_split(
             pds.dung_defender_awake_convo_on_entry(p, g)
-                && g.get_scene_name(p)
+                && (g
+                    .get_scene_name(p)
                     .is_some_and(|s| s.starts_with("Waterways_15"))
+                    || ss.all_scene_names().contains(&"Waterways_15"))
                 && g.camera_target_destination(p).is_some_and(|v| v.x < 29.5),
         ),
         Split::MetEmilitia => should_split(g.met_emilitia(p).is_some_and(|m| m)),
@@ -5184,7 +5187,7 @@ pub fn splits(
 ) -> SplitterAction {
     #[cfg(debug_assertions)]
     pds.get_game_state(prc, g);
-    let a1 = continuous_splits(s, prc, g, pds, sds).or_else(|| {
+    let a1 = continuous_splits(s, prc, g, ss, pds, sds).or_else(|| {
         let pair = ss.pair();
         let a2 = if !ss.split_this_transition {
             transition_once_splits(s, &pair, prc, g, pds)
